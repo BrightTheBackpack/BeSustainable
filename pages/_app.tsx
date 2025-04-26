3b844c078a7470582037ffdfa90872ec844b1106
BrightTheBackpack <100brightli@gmail.com>
2025-04-26 10:17:48 -0700

    Init

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import type { AppProps } from 'next/app'
+
+import Meta from '#/components/meta'
+import ColorSwitcher from '#/components/color-switcher'
+import { ThemeProvider } from 'theme-ui'
+import theme from '#/lib/theme'
+import { MDXProvider, useMDXComponents } from '@mdx-js/react'
+import { useThemedStylesWithMdx } from '@theme-ui/mdx'
+
+const mdxComponents = {}
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const componentsWithStyles = useThemedStylesWithMdx(
+    useMDXComponents(mdxComponents),
+  )
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Meta />
+      <ColorSwitcher />
+      
+    </ThemeProvider>
+  )
+}
+
+export default App
